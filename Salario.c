9aa843776f017cda35bc718d1245971b589aa0de
php <php__@tutamail.com>
2024-04-03 16:53:46 -0300

    Corrige o calculo mensal e diario

--- Salario.c
+++ Salario.c
@@ -2,20 +2,23 @@
 
 int main(void) {
   //pega as informações do salario
-  float salario, refeicao, reajuste, lucro, diferenca, hora = 0;
+  float salario, refeicao, reajuste, lucro, diferenca, hora, refeicaomes, dia = 0;
   printf("\nInforme o salário: ");
   scanf("%f", &salario);
   printf("\nInforme o novo valor do vale refeição: ");
   scanf("%f", &refeicao);
   printf("\nInforme o reajuste: ");
   scanf("%f", &reajuste);
+  //calcula o vale refeição
+  refeicaomes = refeicao * 20;
   //calcula o reajuste
-  diferenca = (salario + refeicao) * reajuste / 100;
+  diferenca = salario * (reajuste / 100);
   //calcula o salario
-  lucro = (salario + refeicao) + diferenca;
+  lucro = (salario + refeicaomes) + diferenca;
   printf("\nSeu salário atual é: %.2f", lucro);
   //calcula o quanto ele ganha por hora
-  hora = lucro / 30;
+  dia = lucro / 20;
+  hora = dia / 6;
   printf("\nPor hora você ganha: %.2f", hora);
   
 }
